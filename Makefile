99d223c81787b32cbc65e716dea410e8ed6af759
Ruthvik Peddawandla <ruthvik.ped94@gmail.com>
2015-04-10 18:34:39 -0700

    EE 450 Final Project

---
+++ Makefile
@@ -0,0 +1,25 @@
+CC = g++
+CPPFLAGS = -Wall -g
+UNIXARGS = -lsocket -lnsl -lresolv
+
+all: server1 server2 server3 client1 client2
+	./server1
+	./server2
+	./server3
+	./client1
+	./client2
+
+server1: server1.cpp 
+	$(CC) $(CPPFLAGS) -o server1 server1.cpp $(UNIXARGS)
+
+server2: server2.cpp
+	$(CC) $(CPPFLAGS) -o server2 server2.cpp $(UNIXARGS)
+
+server3: server3.cpp
+	$(CC) $(CPPFLAGS) -o server3 server3.cpp $(UNIXARGS)
+
+client1: client1.cpp
+	$(CC) $(CPPFLAGS) -o client1 client1.cpp $(UNIXARGS)
+
+client2: client2.cpp
+	$(CC) $(CPPFLAGS) -o client2 client2.cpp $(UNIXARGS)
---
+++ server2.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	map<string, string> dataSet; 
+	//read data from server1.txt and store in Map
+	//this converts data in .txt file to standard input
+	freopen("server2.txt", "r", stdin);
+
+	//read into map
+	while (!cin.fail()) {
+		string key, value;
+		cin >> key >> value;
+		dataSet.insert(make_pair(key, value));
+	}
+
+	
+
+	return 0;
+}
